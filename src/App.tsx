c4bed159fd1bf42313cd771f9858fbbf6b6cdcd3
Ryan Kim <ryank9462@gmail.com>
2022-07-20 22:01:31 -0500

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  background-color: hsl(226, 43%, 10%);
+  height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <StyledDiv></StyledDiv>;
 }
 
 export default App;
